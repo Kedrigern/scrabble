2d0b7d1532b16d8296d1973fb86686bd99e5d265
Ondřej Profant <ondrej.profant@gmail.com>
2011-12-20 20:40:46 +0100

    Aded test to makedoc script

--- scripts/makeDoc.sh
+++ scripts/makeDoc.sh
@@ -7,14 +7,27 @@ cd ../Scrabble/bin/Release/
 
 if [ -e Scrabble.exe -a -e Scrabble.xml ]; then :
 else
-	echo "There aren't necesary files.";
+	echo "FAIL: There aren't necesary files.";
 	exit 1;
 fi;
 
 # generate doc
 monodocer -importslashdoc:Scrabble.xml -assembly:Scrabble.exe -path:doc -pretty;
 
+if [ -d doc ]; then :
+else
+        echo "FAIL: monodocer don't make doc directory.";
+        exit 1;
+fi;
+
 monodocs2html -source:doc/ -dest:htmldoc;
+
+if [ -d '"htmldoc"' ]; then :
+else
+        echo "FAIL: monodocs2html don't make htmldoc directory.";
+        exit 1;
+fi;
+
 
 mv '"htmldoc"' htmldoc
 rm -rf ../../../DOC-CS/htmldoc/
